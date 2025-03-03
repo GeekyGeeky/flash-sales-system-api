e04c9bd965effedcc3cad20af928ee552c32b458
GeekyGeeky <therealolushola@gmail.com>
2025-03-03 02:38:28 +0100

    refactor: attached `errorHandler` middleware to express app
    * added global route catch to display 404 error  message
    
    * refactor imports

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express, { Application } from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import compression from 'compression';
-import morgan from 'morgan';
+import express, { Application } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import morgan from "morgan";
+import { errorHandler } from "./middleware/error.middleware";
 
 const app: Application = express();
 
@@ -15,6 +16,18 @@ app.use(helmet()); // Set security HTTP headers
 app.use(cors()); // Enable CORS
 app.use(compression()); // Compress responses
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
+
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    status: "error",
+    message: "Route not found",
+  });
+});
+
+// Global error handler
+app.use(errorHandler);
 
 export default app;
