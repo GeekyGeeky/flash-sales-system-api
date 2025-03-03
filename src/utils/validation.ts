4ee5f728e5c7b825aa8c7b4ed3973117af73591a
GeekyGeeky <therealolushola@gmail.com>
2025-03-03 03:04:29 +0100

    feat: added auth validation schema

---
+++ src/utils/validation.ts
@@ -0,0 +1,13 @@
+import Joi from "joi";
+
+// Auth validation schemas
+export const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(50).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(8).required(),
+});
+
+export const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
